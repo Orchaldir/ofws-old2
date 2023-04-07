fd55c27cad9d561b692b2f64352a3a5c95a51752
Orchaldir <orchaldir@gmail.com>
2023-04-07 20:17:05 +0200

    [#8] Improve RenderingDemo

--- app/src/main/kotlin/ofws/app/demo/RenderingDemo.kt
+++ app/src/main/kotlin/ofws/app/demo/RenderingDemo.kt
@@ -9,32 +9,23 @@ class RenderingDemo : TileApplication(50, 20, 22, 32) {
 
     override fun start(primaryStage: Stage) {
         init(primaryStage, "Rendering Demo")
-        create()
-    }
-
-    private fun create() {
         render()
     }
 
     private fun render() {
-
-        with(this.renderer) {
+        with(renderer) {
             setColor(Color.RED)
             setFont(32)
             renderUnicode("@", 100, 100)
             renderUnicode("🌳", 200, 200)
             setColor(Color.BLUE)
             renderRectangle(400, 300, 100, 200)
-            setColor(Color.GREEN)
         }
 
         with(tileRenderer) {
             renderFullTile(Color.GREEN, 5, 10)
             renderFullTile(Color.BLUE, 6, 10)
             renderUnicodeTile("@", Color.RED, 5, 11, 2)
-        }
-
-        with(tileRenderer) {
             renderText("This is a test.", Color.WHITE, 0, 0, 1)
         }
     }
