011d736e129bca3461dae31ce35b7446fae564a1
Orchaldir <orchaldir@gmail.com>
2023-04-08 10:36:05 +0200

    [#8] More tests

--- ofws-core/src/test/kotlin/ofws/core/render/TileRendererTest.kt
+++ ofws-core/src/test/kotlin/ofws/core/render/TileRendererTest.kt
@@ -34,4 +34,62 @@ class TileRendererTest {
         }
     }
 
+    @Nested
+    inner class RenderUnicodeTile {
+
+        @Test
+        fun `Render unicode tile with default size`() {
+            tileRenderer.renderUnicodeTile("@", Color.GREEN, 2, 3)
+
+            verifySequence {
+                renderer.setColor(Color.GREEN)
+                renderer.setFont(40)
+                renderer.renderUnicode("@",  175, 340)
+            }
+        }
+
+        @Test
+        fun `Render unicode tile with size`() {
+            tileRenderer.renderUnicodeTile("?", Color.GREEN, 2, 3, 4)
+
+            verifySequence {
+                renderer.setColor(Color.GREEN)
+                renderer.setFont(160)
+                renderer.renderUnicode("?",  220, 400)
+            }
+        }
+    }
+
+    @Nested
+    inner class RenderText {
+
+        @Test
+        fun `Render text with default size`() {
+            tileRenderer.renderText("Test", Color.BLUE, 0, 1)
+
+            verifySequence {
+                renderer.setColor(Color.BLUE)
+                renderer.setFont(40)
+                renderer.renderUnicode("T",  115, 260)
+                renderer.renderUnicode("e",  145, 260)
+                renderer.renderUnicode("s",  175, 260)
+                renderer.renderUnicode("t",  205, 260)
+            }
+        }
+
+        @Test
+        fun `Render unicode tile with size`() {
+            tileRenderer.renderText("Test", Color.BLUE, 0, 1, 2)
+
+            verifySequence {
+                renderer.setColor(Color.BLUE)
+                renderer.setFont(80)
+                renderer.renderUnicode("T",  130, 280)
+                renderer.renderUnicode("e",  190, 280)
+                renderer.renderUnicode("s",  250, 280)
+                renderer.renderUnicode("t",  310, 280)
+            }
+        }
+    }
+
 }
