41b1905622e7ba9ad5d210686df7ecd01e56657c
Orchaldir <orchaldir@gmail.com>
2023-04-09 20:36:07 +0200

    [#5] Improve SlopeTest

--- ofws-math/src/test/kotlin/ofws/math/SlopeTest.kt
+++ ofws-math/src/test/kotlin/ofws/math/SlopeTest.kt
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
+private const val N = 10
+
 class SlopeTest {
 
     val slope0 = Slope(1, 0)
@@ -14,27 +16,23 @@ class SlopeTest {
     inner class CalculateTopX {
         @Test
         fun `Test 0 degree slope`() {
-            for (i in 0..10) {
+            for (i in 0..N) {
                 test(slope0, i, 0)
             }
         }
 
         @Test
         fun `Test 45 degree slope`() {
-            for (i in 0..10) {
+            for (i in 0..N) {
                 test(slope45, i, i)
             }
         }
 
         @Test
         fun `Test another slope`() {
-            test(slope, 0, 0)
-            test(slope, 1, 1)
-            test(slope, 2, 1)
-            test(slope, 3, 2)
-            test(slope, 4, 2)
-            test(slope, 5, 3)
-            test(slope, 6, 3)
+            for (i in 0..N) {
+                test(slope, i, (i + 1) / 2)
+            }
         }
 
         private fun test(slope: Slope, localX: Int, result: Int) {
@@ -46,32 +44,55 @@ class SlopeTest {
     inner class CalculateBottomX {
         @Test
         fun `Test 0 degree slope`() {
-            for (i in 0..10) {
+            for (i in 0..N) {
                 test(slope0, i, 0)
             }
         }
 
         @Test
         fun `Test 45 degree slope`() {
-            for (i in 0..10) {
+            for (i in 0..N) {
                 test(slope45, i, i)
             }
         }
 
         @Test
         fun `Test another slope`() {
-            test(slope, 0, 0)
-            test(slope, 1, 0)
-            test(slope, 2, 1)
-            test(slope, 3, 1)
-            test(slope, 4, 2)
-            test(slope, 5, 2)
-            test(slope, 6, 3)
-            test(slope, 7, 3)
+            for (i in 0..N) {
+                test(slope, i, i / 2)
+            }
         }
 
         private fun test(slope: Slope, localX: Int, result: Int) {
             assertEquals(result, slope.calculateBottomX(localX))
+        }
+    }
+
+    @Nested
+    inner class CreateSlopeThroughTopLeft {
+        @Test
+        fun `Create Slope Through Top Left`() {
+            test(1, 0, 1, 1)
+            test(1, 1, 1, 3)
+            test(2, 0, 3, 1)
+        }
+
+        private fun test(x: Int, y: Int, resultX: Int, resultY: Int) {
+            assertEquals(Slope(resultX, resultY), createSlopeThroughTopLeft(x, y))
+        }
+    }
+
+    @Nested
+    inner class CreateSlopeThroughTopRight {
+        @Test
+        fun `Create Slope Through Top Right`() {
+            test(1, 0, 3, 1)
+            test(1, 1, 3, 3)
+            test(2, 0, 5, 1)
+        }
+
+        private fun test(x: Int, y: Int, resultX: Int, resultY: Int) {
+            assertEquals(Slope(resultX, resultY), createSlopeThroughTopRight(x, y))
         }
     }
 }
