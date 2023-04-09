3d6be2c7edcd6d665a25bf03ddec8594c00ff0de
Orchaldir <orchaldir@gmail.com>
2023-04-09 20:24:09 +0200

    [#5] Improve SizeTest

--- ofws-math/src/test/kotlin/ofws/math/SizeTest.kt
+++ ofws-math/src/test/kotlin/ofws/math/SizeTest.kt
@@ -18,31 +18,19 @@ class SizeTest {
 
     @Nested
     inner class GetPosition {
-        @Test
-        fun `Convert x & y inside the size to a position`() {
-            testGetPosition(0, 0, 0)
-            testGetPosition(1, 1, 0)
-            testGetPosition(2, 0, 1)
-            testGetPosition(3, 1, 1)
-            testGetPosition(4, 0, 2)
-            testGetPosition(5, 1, 2)
-        }
-
-        private fun testGetPosition(index: Int, x: Int, y: Int) {
+        @ParameterizedTest(name = "index={0} x={1} y={2}")
+        @MethodSource("ofws.math.SizeTest#inside")
+        fun `Convert x & y inside the size to a position`(index: Int, x: Int, y: Int) {
             assertEquals(Position(index), size.getPosition(x, y))
         }
     }
 
     @Nested
     inner class GetPositionIfInside {
-        @Test
-        fun `Return a position, if inside`() {
-            testGetPosition(0, 0, 0)
-            testGetPosition(1, 1, 0)
-            testGetPosition(2, 0, 1)
-            testGetPosition(3, 1, 1)
-            testGetPosition(4, 0, 2)
-            testGetPosition(5, 1, 2)
+        @ParameterizedTest(name = "index={0} x={1} y={2}")
+        @MethodSource("ofws.math.SizeTest#inside")
+        fun `Return a position, if inside`(index: Int, x: Int, y: Int) {
+            assertEquals(Position(index), size.getPositionIfInside(x, y))
         }
 
         @ParameterizedTest(name = "x={0} y={1}")
@@ -50,24 +38,20 @@ class SizeTest {
         fun `Return null if outside`(x: Int, y: Int) {
             assertNull(size.getPositionIfInside(x, y))
         }
-
-        private fun testGetPosition(index: Int, x: Int, y: Int) {
-            assertEquals(Position(index), size.getPositionIfInside(x, y))
-        }
     }
 
     @Nested
     inner class IsInside {
 
-        @ParameterizedTest(name = "x={0} y={1}")
+        @ParameterizedTest(name = "index={0} x={1} y={2}")
         @MethodSource("ofws.math.SizeTest#inside")
-        fun `Test x & y with inside`(x: Int, y: Int) {
+        fun `Test x & y with inside`(index: Int, x: Int, y: Int) {
             assertTrue(size.isInside(x, y))
         }
 
-        @ParameterizedTest(name = "x={0} y={1}")
+        @ParameterizedTest(name = "index={0} x={1} y={2}")
         @MethodSource("ofws.math.SizeTest#inside")
-        fun `Test position with inside`(x: Int, y: Int) {
+        fun `Test position with inside`(index: Int, x: Int, y: Int) {
             assertTrue(size.isInside(size.getPosition(x, y)))
         }
 
@@ -102,12 +86,12 @@ class SizeTest {
     companion object {
         @JvmStatic
         fun inside() = listOf(
-            Arguments.of(0, 0),
-            Arguments.of(1, 0),
-            Arguments.of(0, 1),
-            Arguments.of(1, 1),
-            Arguments.of(0, 2),
-            Arguments.of(1, 2),
+            Arguments.of(0, 0, 0),
+            Arguments.of(1, 1, 0),
+            Arguments.of(2, 0, 1),
+            Arguments.of(3, 1, 1),
+            Arguments.of(4, 0, 2),
+            Arguments.of(5, 1, 2),
         )
 
         @JvmStatic
