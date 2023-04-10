31e196a15efa24ce70fc7f16d496a463dbe27839
Orchaldir <orchaldir@gmail.com>
2023-04-10 20:25:42 +0200

    [#5] Add OctantTest

--- ofws-math/src/main/kotlin/ofws/math/Octant.kt
+++ ofws-math/src/main/kotlin/ofws/math/Octant.kt
@@ -10,15 +10,15 @@ enum class Octant {
     SOUTH_WEST,
     WEST,
     NORTH_WEST;
-}
 
-fun Octant.getGlobal(originX: Int, originY: Int, x: Int, y: Int) = when (this) {
-    Octant.NORTH -> Pair(originX + y, originY + x)
-    Octant.NORTH_EAST -> Pair(originX + x, originY + y)
-    Octant.EAST -> Pair(originX + x, originY - y)
-    Octant.SOUTH_EAST -> Pair(originX + y, originY - x)
-    Octant.SOUTH -> Pair(originX - y, originY - x)
-    Octant.SOUTH_WEST -> Pair(originX - x, originY - y)
-    Octant.WEST -> Pair(originX - x, originY + y)
-    Octant.NORTH_WEST -> Pair(originX - y, originY + x)
+    fun getGlobal(originX: Int, originY: Int, x: Int, y: Int) = when (this) {
+        NORTH -> Pair(originX + y, originY + x)
+        NORTH_EAST -> Pair(originX + x, originY + y)
+        EAST -> Pair(originX + x, originY - y)
+        SOUTH_EAST -> Pair(originX + y, originY - x)
+        SOUTH -> Pair(originX - y, originY - x)
+        SOUTH_WEST -> Pair(originX - x, originY - y)
+        WEST -> Pair(originX - x, originY + y)
+        NORTH_WEST -> Pair(originX - y, originY + x)
+    }
 }
---
+++ ofws-math/src/test/kotlin/ofws/math/OctantTest.kt
@@ -0,0 +1,57 @@
+package ofws.math
+
+import ofws.math.Octant.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class OctantTest {
+
+    @Nested
+    inner class GetGlobal {
+        @Test
+        fun `Test North`() {
+            test(NORTH, 11, 22)
+        }
+
+        @Test
+        fun `Test North East`() {
+            test(NORTH_EAST, 12, 21)
+        }
+
+        @Test
+        fun `Test East`() {
+            test(EAST, 12, 19)
+        }
+
+        @Test
+        fun `Test South East`() {
+            test(SOUTH_EAST, 11, 18)
+        }
+
+        @Test
+        fun `Test South`() {
+            test(SOUTH, 9, 18)
+        }
+
+        @Test
+        fun `Test South West`() {
+            test(SOUTH_WEST, 8, 19)
+        }
+
+        @Test
+        fun `Test West`() {
+            test(WEST, 8, 21)
+        }
+
+        @Test
+        fun `Test NORTH West`() {
+            test(NORTH_WEST, 9, 22)
+        }
+
+        private fun test(octant: Octant, x: Int, y: Int) {
+            assertEquals(Pair(x, y), octant.getGlobal(10, 20, 2, 1))
+        }
+    }
+
+}
