bda74a3cf7e94f1e83d8ca8b0aa786b3ec0cfa72
Orchaldir <orchaldir@gmail.com>
2023-04-10 17:13:31 +0200

    [#5] Refactor test

--- ofws-math/src/test/kotlin/ofws/math/fov/ShadowCastingTest.kt
+++ ofws-math/src/test/kotlin/ofws/math/fov/ShadowCastingTest.kt
@@ -9,93 +9,94 @@ import org.junit.jupiter.api.Test
 class ShadowCastingTest {
 
     private val algo = ShadowCasting()
+    private lateinit var map: TileMap<Boolean>
 
     @Test
     fun `Range 1`() {
-        val map = TileMapBuilder(10, 10, false).build()
-        val config = createConfig(map, 3, 3, 1)
+        map = TileMapBuilder(10, 10, false).build()
+        val config = createConfig(3, 3, 1)
 
         val visible = algo.calculateVisibleCells(config)
 
         assertEquals(9, visible.size)
 
-        assertRectangle(visible, map, 2, 4, 2, 4)
+        assertRectangle(visible, 2, 4, 2, 4)
     }
 
     @Test
     fun `Range 2`() {
-        val map = TileMapBuilder(10, 10, false).build()
-        val config = createConfig(map, 4, 4, 2)
+        map = TileMapBuilder(10, 10, false).build()
+        val config = createConfig(4, 4, 2)
 
         val visible = algo.calculateVisibleCells(config)
 
         assertEquals(25, visible.size)
 
-        assertRectangle(visible, map, 2, 6, 2, 6)
+        assertRectangle(visible, 2, 6, 2, 6)
     }
 
     @Test
     fun `A wall blocks the field of view`() {
-        val map = TileMapBuilder(7, 7, false)
+        map = TileMapBuilder(7, 7, false)
             .addRectangle(1, 1, 5, 5, true)
             .build()
-        val config = createConfig(map, 4, 4, 10)
+        val config = createConfig(4, 4, 10)
 
         val visible = algo.calculateVisibleCells(config)
 
         assertEquals(25, visible.size)
 
-        assertRectangle(visible, map, 1, 5, 1, 5)
+        assertRectangle(visible, 1, 5, 1, 5)
     }
 
     @Test
     fun `A single wall next to the watcher`() {
-        val map = TileMapBuilder(10, 10, false)
+        map = TileMapBuilder(10, 10, false)
             .addBorder(true)
             .setTile(5, 4, true)
             .build()
-        val config = createConfig(map, 4, 4, 10)
+        val config = createConfig(4, 4, 10)
 
         val visible = algo.calculateVisibleCells(config)
 
         // 1.column
-        notVisible(visible, map, 6, 3)
-        notVisible(visible, map, 6, 4)
-        notVisible(visible, map, 6, 5)
+        notVisible(visible, 6, 3)
+        notVisible(visible, 6, 4)
+        notVisible(visible, 6, 5)
 
         // 2.column
-        notVisible(visible, map, 7, 2)
-        notVisible(visible, map, 7, 3)
-        notVisible(visible, map, 7, 4)
-        notVisible(visible, map, 7, 5)
-        notVisible(visible, map, 7, 6)
+        notVisible(visible, 7, 2)
+        notVisible(visible, 7, 3)
+        notVisible(visible, 7, 4)
+        notVisible(visible, 7, 5)
+        notVisible(visible, 7, 6)
 
         // 3.column
-        notVisible(visible, map, 8, 1)
-        notVisible(visible, map, 8, 2)
-        notVisible(visible, map, 8, 3)
-        notVisible(visible, map, 8, 4)
-        notVisible(visible, map, 8, 5)
-        notVisible(visible, map, 8, 6)
-        notVisible(visible, map, 8, 7)
+        notVisible(visible, 8, 1)
+        notVisible(visible, 8, 2)
+        notVisible(visible, 8, 3)
+        notVisible(visible, 8, 4)
+        notVisible(visible, 8, 5)
+        notVisible(visible, 8, 6)
+        notVisible(visible, 8, 7)
 
         // 4.column
-        notVisible(visible, map, 9, 0)
-        notVisible(visible, map, 9, 1)
-        notVisible(visible, map, 9, 2)
-        notVisible(visible, map, 9, 3)
-        notVisible(visible, map, 9, 4)
-        notVisible(visible, map, 9, 5)
-        notVisible(visible, map, 9, 6)
-        notVisible(visible, map, 9, 7)
-        notVisible(visible, map, 9, 8)
+        notVisible(visible, 9, 0)
+        notVisible(visible, 9, 1)
+        notVisible(visible, 9, 2)
+        notVisible(visible, 9, 3)
+        notVisible(visible, 9, 4)
+        notVisible(visible, 9, 5)
+        notVisible(visible, 9, 6)
+        notVisible(visible, 9, 7)
+        notVisible(visible, 9, 8)
     }
 
-    private fun notVisible(visible: Set<Position>, map: TileMap<Boolean>, x: Int, y: Int) {
+    private fun notVisible(visible: Set<Position>, x: Int, y: Int) {
         assertFalse(visible.contains(map.size.getPosition(x, y)))
     }
 
-    private fun assertRectangle(visible: Set<Position>, map: TileMap<Boolean>, x0: Int, x1: Int, y0: Int, y1: Int) {
+    private fun assertRectangle(visible: Set<Position>, x0: Int, x1: Int, y0: Int, y1: Int) {
         for (x in x0..x1) {
             for (y in y0..y1) {
                 assertTrue(visible.contains(map.size.getPosition(x, y)))
@@ -103,7 +104,7 @@ class ShadowCastingTest {
         }
     }
 
-    private fun createConfig(map: TileMap<Boolean>, x: Int, i: Int, range: Int) =
+    private fun createConfig(x: Int, i: Int, range: Int) =
         FovConfig(map.size, map.size.getPosition(x, i), range) { p -> map.terrainList[p.index] }
 
 }
