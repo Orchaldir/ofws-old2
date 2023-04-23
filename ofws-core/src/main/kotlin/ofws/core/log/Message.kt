e5adc868603841544c1b3c214bc111b7ea3515ca
Orchaldir <orchaldir@gmail.com>
2023-04-23 11:05:48 +0200

    [#6] Add MessageLog

---
+++ ofws-core/src/main/kotlin/ofws/core/log/Message.kt
@@ -0,0 +1,21 @@
+package ofws.core.log
+
+import ofws.ecs.EcsState
+import ofws.ecs.Entity
+
+enum class MessageType {
+    INFO,
+    WARN,
+}
+
+data class Message(val text: String, val type: MessageType)
+
+fun inform(text: String) = Message(text.capitalize(), MessageType.INFO)
+
+fun inform(state: EcsState, text: String, entity: Entity) = inform(text.format(getText(state, entity)))
+
+fun warn(text: String) = Message(text.capitalize(), MessageType.WARN)
+
+fun warn(state: EcsState, text: String, entity: Entity) = warn(text.format(getText(state, entity)))
+
+fun getText(state: EcsState, entity: Entity) = "entity $entity"
